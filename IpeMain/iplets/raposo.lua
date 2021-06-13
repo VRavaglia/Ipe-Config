2ecbecac0d82e130d5c4a6ae87173ba3efaa42aa
Victor Raposo <victor.ravaglia@hotmail.com>
2021-06-12 21:39:09 -0300

    Line width and more components

--- IpeMain/iplets/raposo.lua
+++ IpeMain/iplets/raposo.lua
@@ -5,3 +5,45 @@ prefs.initial = {
     angle_size = 45,    -- degrees
     grid_visible = true,
   }
+
+prefs.initial_attributes = {
+  pathmode = "stroked",
+  stroke = "black",
+  fill = "white",
+  pen = "heavier",
+  dashstyle = "normal",
+  farrowshape = "arrow/normal(spx)",
+  rarrowshape = "arrow/normal(spx)",
+  farrowsize = "normal",
+  rarrowsize = "normal",
+  farrow = false,
+  rarrow = false,
+  symbolsize = "small",
+  textsize = "normal",
+  textstyle = "normal",
+  transformabletext = false,
+  splinetype = "bspline",
+  horizontalalignment = "left",
+  verticalalignment = "baseline",
+  pinned = "none",
+  transformations = "affine",
+  linejoin = "normal",
+  linecap = "square",
+  fillrule = "normal",
+  markshape = "mark/disk(sx)",
+  tiling = "normal",
+  gradient = "normal",
+  opacity = "opaque",
+  strokeopacity = "opaque",
+}
+
+prefs.snap = { 
+  vertex = true,
+  ctrlpoints = false,
+  boundary = false,
+  intersection = false,
+  grid = true,
+  angle = false,
+  custom = false,
+  autoangle = false
+}
